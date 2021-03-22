b246a293f60aae7874de3a7e29211986a958f5e9
andrew2020wit <andrew2020wit@gmail.com>
2021-03-22 16:58:42 +0200

    done

--- app.js
+++ app.js
@@ -1,9 +1,50 @@
 const WebSocket = require("ws");
 
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("logdb", "root", "12345", {
+  dialect: "mysql",
+  host: "localhost",
+  port: "3306",
+});
+
+const InputMessage = sequelize.define("input", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  message: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+  })
+  .catch((err) => console.log(err));
+
+InputMessage.findAll({ raw: true })
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((err) => console.log(err));
+
 const webSocketServer = new WebSocket.Server({ port: 8081 });
 
 webSocketServer.on("connection", function connection(webSocket) {
   webSocket.on("message", function incoming(message) {
     console.log("received: %s", message);
+
+    InputMessage.create({
+      message: message,
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
   });
 });
--- readme.md
+++ readme.md
@@ -1,6 +1,9 @@
-# Node.js + websocket(ws) + MySQL
+# Node.js + websocket(ws) + MySQL (Sequelize)
 
 Запись входящих сообщений (ws, text) в MySQL
+
+1. index.html отправляет сообщения через setInterval, сразу после открытия (с пользователем не взаимодействует)
+2. сервер при старте показывает все прежние сообщения, и записывает новые в MySQL
 
 ## based on
 
