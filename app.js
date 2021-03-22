086716066819d3b55bc559f4afba03c586538b56
andrew2020wit <andrew2020wit@gmail.com>
2021-03-22 16:08:48 +0200

    to console

--- app.js
+++ app.js
@@ -6,15 +6,4 @@ webSocketServer.on("connection", function connection(webSocket) {
   webSocket.on("message", function incoming(message) {
     console.log("received: %s", message);
   });
-  let count = 0;
-  sender();
-  function sender() {
-    count++;
-    webSocket.send(`server interval send #${count}`);
-    if (count < 5) {
-      setTimeout(() => sender(), 1000);
-    } else {
-      webSocket.close();
-    }
-  }
 });
